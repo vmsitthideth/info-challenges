0548c87b39cdd681b5c8f0b2e288689ebde0d3c7
Vichit Mike Sitthideth <vmsitth@gmail.com>
2015-10-25 23:18:59 -0700

    add video link and text boxes

--- reviewer/js/app.js
+++ reviewer/js/app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+Parse.initialize("2qfsOTARU0KQTMTfqqVvaSuCxmB1sqEuDWbKQGee", "9zkkgo0emUFr2RZaqzm7UWbyiecdENYehaeTF427");
+
+var Review = Parse.Object.extend("Review");
+
+var myReview = new Review();
 $('#star').raty({
-	score: 3
+	
 });
