fe4d4cdfb35a140c68225b17d6a6a6d718866546
Vichit Mike Sitthideth <vmsitth@gmail.com>
2015-10-27 03:51:10 -0700

    add visuals

--- reviewer/js/app.js
+++ reviewer/js/app.js
@@ -1,24 +1,26 @@
 'use strict';
 
-Parse.initialize("2qfsOTARU0KQTMTfqqVvaSuCxmB1sqEuDWbKQGee", "9zkkgo0emUFr2RZaqzm7UWbyiecdENYehaeTF427");
 
-$('.star').raty({	
+$(document).ready(function() {
+
+	Parse.initialize("2qfsOTARU0KQTMTfqqVvaSuCxmB1sqEuDWbKQGee", "9zkkgo0emUFr2RZaqzm7UWbyiecdENYehaeTF427");
+
+	var Review = Parse.Object.extend('Review');
+	var myReview = new Review();
+
+	$('.star').raty({	
+	});
+
+
+	$('#myForm').submit(function() {
+		
+		var rating = $('.star').raty('score');
+		var title = $('#title').val();
+		var review = $('#review').val();	
+
+		myReview.set('rating', rating);
+		myReview.set('title', title);
+		myReview.set('review', review);
+		myReview.save();
+	});
 });
-
-var Review = Parse.Object.extend("Review");
-
-var myReview = new Review();
-
-$('myForm').submit(function() {
-var rating = $('#star').raty('score');
-var title = $('#title').val;
-var review = $('#review').val;
-myReview.set("rating", rating);
-myReview.set("title", title);
-myReview.set("review", review);
-
-})
-
-
-//myReview.save();
-
