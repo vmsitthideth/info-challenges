52a77a22e2e586ebdd5eb9fa50895997e3f4a5c6
Vichit Mike Sitthideth <vmsitth@uw.edu>
2015-10-28 17:14:19 -0700

    Still Deleting Top Only

--- reviewer/js/app.js
+++ reviewer/js/app.js
@@ -1,5 +1,7 @@
-'use strict';
+// Vichit Mike Sitthideth
+// INFO 343 B
 
+'use strict';
 
 $(document).ready(function() {
 
@@ -30,52 +32,57 @@ $(document).ready(function() {
 		location.reload();
 		return false;
 	});
-
+	
 	//  Loops through each ID, pulls categories, and displays each category
-	query.find({
-		success: function(results) {
-			var totalRating = 0;
-			var userCount = 0;
-			results.forEach(function(data) {
-				var newTitle = "<h2>" + data.get('title') + "</h2>";
-				var newReview = data.get('review');
-				var newRating = $("<div></div>").raty({
-						readOnly: true,
-						score: data.get('rating')
+	var getData = function() {
+		query.find({
+			success: function(results) {
+				var totalRating = 0;
+				var userCount = 0;
+				results.forEach(function(data) {
+					var newTitle = "<h2>" + data.get('title') + "</h2>";
+					var newReview = data.get('review');
+					var newRating = $("<div></div>").raty({
+							readOnly: true,
+							score: data.get('rating')
 					});
-				var helpful = data.get('upVote');
-				var unhelpful = data.get('downVote');
+					var helpful = data.get('upVote');
+					var unhelpful = data.get('downVote');
 
-				var userReview = "<div id='" + data.id + "'>" + newTitle + '<br>' + '<p>' + newReview + '</p></div>';
+					var userReview = "<div id='" + data.objectID + "'>" + newTitle + '<br>' + '<p>' + newReview + '</p></div>';
 
-				var voteTotal = helpful + unhelpful;
+					var voteTotal = helpful + unhelpful;
 
-				var useful = "<p id ='votes'>" + helpful + " out of " + voteTotal + " people agree</p>";
+					var useful = "<p id ='votes'>" + helpful + " out of " + voteTotal + " people agree</p>";
 
-				var section = $("<div class='section'></div>");
-				var erase = "<button type='button' class='btn-default erase'><i class='fa fa-eraser'></i></button>";
-				var thumbUp = "<button type='button' class='btn-default btn-vote thumbUp'><i class='fa fa-thumbs-up'></i></button>";
-				var thumbDown = "<button type='button' class='btn-default btn-vote thumbDown'><i class='fa fa-thumbs-down'></i></button>";
+					var section = $("<div class='section'></div>");
 
-				$('#submittedReview').append(section);
-				$(section).append(thumbDown + thumbUp);
-				$(section).append(newRating);
-				$(section).append("<div id='userReview'>" + userReview + "</div>");
-				totalRating += data.get('rating');
-				userCount++;
-				$(section).append(useful);
-				$(section).append(erase);
-			})
-			$('.averageStar').raty({
-				readOnly: true,
-				score: totalRating / userCount
-			});
-		}
-	})
+					var erase = "<button type='button' class='btn-default erase'><i class='fa fa-eraser'></i></button>";
+
+					var thumbUp = "<button type='button' class='btn-default btn-vote thumbUp'><i class='fa fa-thumbs-up'></i></button>";
+
+					var thumbDown = "<button type='button' class='btn-default btn-vote thumbDown'><i class='fa fa-thumbs-down'></i></button>";
+
+					$('#submittedReview').append(section);
+					$(section).append(newRating);
+					$(section).append(thumbDown + thumbUp);
+					$(section).append("<div id='userReview'>" + userReview + "</div>");
+					totalRating += data.get('rating');
+					userCount++;
+					$(section).append(useful);
+					$(section).append(erase);
+				})
+				$('.averageStar').raty({
+					readOnly: true,
+					score: totalRating / userCount
+				});
+			}
+		})
+	};
 
 	//  Function that enables the deletion of a review upon clicking the delete icon
 	$(document).on('click', '.erase', function() {
-		var parent = $(this).parent();
+		var parent = $('this').parent();
 		var id = $(parent).attr("id");
 		var query = new Parse.Query(Review);
 		query.equalTo("objectID", id);
@@ -96,7 +103,7 @@ $(document).ready(function() {
 
 	//  Function that allows for thumbs up rating
 	$(document).on('click', '.thumbUp', function() {
-		var parent = $(this).parent();
+		var parent = $('this').parent();
 		var id = $(parent).attr("id");
 		var query = new Parse.Query(Review);
 		query.equalTo("objectID", id);
@@ -118,7 +125,7 @@ $(document).ready(function() {
 
 	//  Function that allows for thumbs down rating
 	$(document).on('click', '.thumbDown', function() {
-		var parent = $(this).parent();
+		var parent = $('this').parent();
 		var id = $(parent).attr("id");
 		var query = new Parse.Query(Review);
 		query.equalTo("objectID", id);
@@ -137,4 +144,7 @@ $(document).ready(function() {
 			}
 		})
 	})
+
+	//	Calls getData to fetch all the reviews
+	getData();
 });
