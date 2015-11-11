dd35747c918f432ab69872fb5bbafc6f647c6a07
Vichit Mike Sitthideth <vmsitth@uw.edu>
2015-11-10 19:12:49 -0800

    added local storage

--- dawg-coffee/js/app.js
+++ dawg-coffee/js/app.js
@@ -22,8 +22,8 @@ angular.module('DawgCoffeeApp', ['ngSanitize', 'ui.router', 'ui.bootstrap'])
 			controller: 'BeanCtrl'
 		})
 		.state('cart', {
-			url: '/cart',
-			templateURL: 'partials/cart.html',
+			url: '/orders/cart',
+			templateUrl: 'partials/cart.html',
 			controller: 'CartCtrl'
 		})
 })
@@ -31,6 +31,7 @@ angular.module('DawgCoffeeApp', ['ngSanitize', 'ui.router', 'ui.bootstrap'])
 	$urlRouterProvider.otherwise('/');
 })
 
+// Controller for Order page
 .controller('OrderCtrl', ['$scope', '$http', '$uibModal', function($scope, $http, $uibModal) {
 		$http.get('data/products.json').then(function(response) {
 			$scope.products = response.data;
@@ -38,40 +39,40 @@ angular.module('DawgCoffeeApp', ['ngSanitize', 'ui.router', 'ui.bootstrap'])
 
 }])
 
-.controller('BeanCtrl', ['$scope', '$http', '$stateParams', '$filter', function($scope, $http, $stateParams, $filter) {
+.controller('BeanCtrl', ['$scope', '$http', '$stateParams', '$filter', 'cartList', function($scope, $http, $stateParams, $filter, cartList) {
 	$http.get('data/products.json').then(function(response) {
 	   	$scope.product = $filter('filter')(response.data, { 
 	      id: $stateParams.id 
 	   	}, true)[0]; 
  	});
+
+ 	$scope.addToCart = function(bean) {
+ 		cartList.saveCart(bean);
+ 		cartList.addToStorage(bean);
+ 	}
 }])
 
-.controller('FormCtrl', ['$scope', function($scope) { 
-
-  $scope.addToCart = function(form){
-  	if(form.$valid) {
-  		alert("Payment info has been submitted!");
-  	}
-  	else {
-  		alert("There was an error submitting your payment. Please check that your information is correct.");
-  	}
-  }
-  
+.controller('CartCtrl', ['$scope', '$http', '$uibModal', function($scope, $http, $uibModal) {
+		$http.get('data/products.json').then(function(response) {
+			$scope.products = response.data;
+		});	
 }])
-
-// .controller('CartCtrl', ['$scope', '$http', '$uibModal', function($scope, $http, $uibModal) {
-// 	$scope.cart = 
-// }])
 
 
 // service for cart list
-.factory('cart', function() {
+.factory('cartList', function() {
 	var service = {};
-	service.cart = [];
-
+	service.cartList = [];
+	if (localStorage.getItem("basket")) {
+		service.cartList = JSON.parse(localStorage.getItem("basket"));
+	}
 	service.saveCart = function(product) {
-		service.cart.push(product);
+		service.cartList.push(product);
+		service.addToStorage();
 	};
+	service.addToStorage = function() {
+		localStorage.setItem("basket", angular.toJson(service.cardList));
+	}
 
 	return service;
 })
